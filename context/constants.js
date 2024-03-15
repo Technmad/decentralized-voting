6d59c4c3257366dc21b2df5b76970e3288b8ddb4
anurag <anuragpathakoffl@gmail.com>
2024-03-15 10:23:14 +0530

    updated constant file for abi calling

--- context/constants.js
+++ context/constants.js
@@ -1,1 +1,9 @@
 // context is folder where real application logic is writtern. API calls to smart contract is made and logic is executed here.
+
+//0x5FbDB2315678afecb367f032d93F642f64180aa3
+
+import voting from "./Create.json";
+
+export const votingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+export const votingAddressABI = voting.abi;
