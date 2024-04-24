9f93fe50901525fbc932c89ecb3567ab2cdae3cb
ironspec07 <ironspec07@gmail.com>
2024-04-24 19:14:21 +0530

    importing pakages

--- pages/allowedVoters.js
+++ pages/allowedVoters.js
@@ -1,4 +1,15 @@
-import React from "react";
+import React , {useCallback,useEffect,useState,useContext} from "react";
+import {useRouter} from "next/router";
+import { useDropzone } from "react-dropzone";
+import Image from "next/image";
+
+import { VotingContext } from "@/context/Voter";
+import Style from '@/styles/allowedVoter.module.css';
+import images from '@/assets';
+import Button from "@/components/Button/Button";
+import Input from "@/components/Input/Input";
+
+
 
 const allowedVoters = () => {
   return <div>allowedVoters</div>;
